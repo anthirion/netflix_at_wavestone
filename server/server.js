a52503eb1f008178ee87c364006b1822a184a7eb
antoinethirion <antoine.thirion@wavestone.com>
2024-01-08 12:13:03 +0100

    les séries sont chargées automatiquement dans la bdd au démarrage du serveur

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const cors = require('cors');
 const express = require('express');
 const connectDB = require('./db');
+const fs = require('node:fs');
 const MongoDBURL = process.env.DATABASE_URL;
 
 // express
@@ -13,7 +14,7 @@ app.use(express.json());
 connectDB(MongoDBURL);
 
 const PORT = 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Build CRUD operations
 const router = require('./routes');
@@ -21,18 +22,24 @@ const Serie = require('./data_models');
 app.use('/netflix_at_wavestone', router);
 
 
+// Add the series automatically to the database
+// Get the data from the json file
+console.log(' Loading all data in the database ...');
+const json_series_file = "../data/series.json"
+fs.readFile(json_series_file, 'utf8', (err, data) => {
+    if (err)    console.error(err);
+    else {
+        var series_data = JSON.parse(data);
+        // Save the series in the mongodb database
+        Serie.collection.insertMany(series_data, (err, r) => {
+            if(err)     console.log("Cannot create new serie: %s", err);
+            else        console.log("Data has been correctly loaded");
+        })
+    }
+})
 
-// Add a new serie automatically to the database
-// var newSerie = new Serie({
-//     id: 1,
-//     id_scriptwriter: 1,
-//     episodes: [1, 2],
-//     description: "This is the serie named Lupin"
-// });
-
-// // Save the new serie
-// newSerie.save(function(err) {
-//     if(err) {
-//       console.log("Can't create new serie: %s", err);
-//     }
-// })
+// Event triggered when CTRL + C is pressed
+process.on('SIGINT', () => {
+    // Delete all saved data in database
+    console.log(' Deleting all data in the database ...');
+});
