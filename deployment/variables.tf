13cdb8f35564ceb423edf676ff35aae19182495a
antoinethirion <antoine.thirion@wavestone.com>
2025-09-21 16:13:33 +0200

    chore: save last gcp deployment config

---
+++ deployment/variables.tf
@@ -0,0 +1,18 @@
+variable "credentials_file" {
+  default = "C:\\Users\\antoine.thirion\\AppData\\Roaming\\gcloud\\application_default_credentials.json"
+}
+variable "project" {
+  default = "wavestone-cto-labapparchi"
+}
+
+variable "region" {
+  default = "europe-west1"
+}
+
+variable "zone" {
+  default = "europe-west1-d"
+}
+
+variable "public_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDe65cDEvmBQ4CUjmP9dNeSSXSBXfygeP8cMC0tFU8AGD3wh5XX89DNj93ldtwcZ7UcbsKaBocsIjGVc8yS6JfLtoaFXdSp/qCL7oEL23I2EaoDZVBuNqOGgwhYVWweHpQ5TWyLC6Oh86TSJV/2nlegxwYxQeXyjFg0XyWZNeFNYK9Ur22xAZyGYU/efjx1Y58whcTep/ouoXohkShfu8QiAZNH7Q2XZXPiRzSfHEphKnhdxPwYrdqkQQ8BUMQg2wWj2n4wTvTOQWvnSslXnTWiRSV64QpBxwLnIe0LYA3fSPZpEGO4pmMt0gMCtsfZ6lUXIu67HlyJzTU4FhnUR2lVLnNUytowiUNoubhouw2nUKr3kj5EbBAe3fWR24rbEQYGsY10omWXoGV97xCPqyjf+WV9JvXUyY4amb7Nu3I7dQhqi9YbXVfu3IVW9RyfN91MQ+/VLA45gzGouVVqJr7TLY5Ps2C/dalHFvLKIWvgVd+99Je8Aaevx4ykuA7r2tGRytcUW8wiq2dmfTsXrf4UGkRHHqc3dedcuUZdRP8LUcUn7mapDJzWxt/Ym11aSnmhIYYt5mVX7r3n5LXkDjIlmybUx4IJN3O7jX8Eo/ajT9+ltbIdmfPzv9IbeyVncRt/DKuHL7L8IJMX8yWPrVy7P3md+6mQj6Qpt7ojfYcvMw== anthirion@W5CG24305HJ"
+}
