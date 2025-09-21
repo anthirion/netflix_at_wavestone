d869245807aea78e86ab92a05d6192fd2e6cabed
antoinethirion <antoine.thirion@wavestone.com>
2025-09-21 16:15:04 +0200

    feat: import data to cosmos db with a script

---
+++ data/importData.ps1
@@ -0,0 +1,23 @@
+$USERNAME = "netflix-db"
+$DBNAME = "netflix-db"
+
+$files = @("episodes.json", "scriptwriters.json", "series.json")
+
+$URI = Read-Host "Please enter Cosmos DB host (see in Azure Portal): "
+$PASSWORD = Read-Host "Please enter primary password string from the Azure Portal: " -MaskInput
+
+foreach ($file in $files) {
+    $baseName = [System.IO.Path]::GetFileNameWithoutExtension($file)
+    $collection = $baseName.Substring(0,1).ToUpper() + $baseName.Substring(1)
+
+    mongoimport --host $URI `
+        --ssl `
+        --username $USERNAME `
+        --password $PASSWORD `
+        --db $DBNAME `
+        --collection $collection `
+        --type json `
+        --writeConcern="{w:0}" `
+        --file $file `
+        --jsonArray
+}
