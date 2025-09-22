dbd984fe394baabc3bb8cb10ee43d3453f98a64d
antoinethirion <antoine.thirion@wavestone.com>
2025-09-22 22:16:15 +0200

    fix: add port when importing data to cosmos DB

--- data/importData.ps1
+++ data/importData.ps1
@@ -3,8 +3,9 @@ $DBNAME = "netflix-db"
 
 $files = @("episodes.json", "scriptwriters.json", "series.json")
 
-$URI = Read-Host "Please enter Cosmos DB host (see in Azure Portal): "
-$PASSWORD = Read-Host "Please enter primary password string from the Azure Portal: " -MaskInput
+$URI = Read-Host "Please enter Cosmos DB host (see in Azure Portal)"
+$PORT = Read-Host "Please enter Cosmos DB port (see in Azure Portal)"
+$PASSWORD = Read-Host "Please enter primary password string from the Azure Portal" -MaskInput
 
 foreach ($file in $files) {
     $baseName = [System.IO.Path]::GetFileNameWithoutExtension($file)
@@ -12,6 +13,7 @@ foreach ($file in $files) {
 
     mongoimport --host $URI `
         --ssl `
+        --port $PORT `
         --username $USERNAME `
         --password $PASSWORD `
         --db $DBNAME `
