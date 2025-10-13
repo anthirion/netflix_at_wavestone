674973f42ba376e5290838a1cf8035eedb4ad069
antoinethirion <antoine.thirion@wavestone.com>
2025-10-13 22:45:57 +0200

    chore: remove database url env variable and move it to terraform code

--- azure/main.tf
+++ azure/main.tf
@@ -1,25 +1,25 @@
-resource "azurerm_cosmosdb_account" "netflix-db" {
-  name                = "netflix-db"
-  location            = local.region
-  resource_group_name = local.rg_name
-  offer_type          = "Standard"
-  kind                = "MongoDB"
+# resource "azurerm_cosmosdb_account" "netflix-db" {
+#   name                = "netflix-db"
+#   location            = local.region
+#   resource_group_name = local.rg_name
+#   offer_type          = "Standard"
+#   kind                = "MongoDB"
 
-  free_tier_enabled          = true
-  automatic_failover_enabled = false
+#   free_tier_enabled          = true
+#   automatic_failover_enabled = false
 
-  consistency_policy {
-    consistency_level       = "BoundedStaleness"
-    max_interval_in_seconds = 300
-    max_staleness_prefix    = 100000
-  }
+#   consistency_policy {
+#     consistency_level       = "BoundedStaleness"
+#     max_interval_in_seconds = 300
+#     max_staleness_prefix    = 100000
+#   }
 
-  # primary region
-  geo_location {
-    location          = local.region
-    failover_priority = 0
-  }
-}
+#   # primary region
+#   geo_location {
+#     location          = local.region
+#     failover_priority = 0
+#   }
+# }
 
 resource "azurerm_container_group" "netflix-api" {
   name                = "netflix-api"
@@ -33,6 +33,10 @@ resource "azurerm_container_group" "netflix-api" {
     image  = "ghcr.io/anthirion/netflix_at_wavestone:latest"
     cpu    = "1"
     memory = "1"
+    
+    environment_variables = {
+      DATABASE_URL = "mongodb://netflix-db:27017"
+    }
 
     ports {
       port     = 443
