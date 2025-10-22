2afc5c389b23e1ac2226dead880a9996afc6076c
antoinethirion <antoine.thirion@wavestone.com>
2025-10-22 15:33:20 +0200

    chore: replace the node js init db script by a bash script

---
+++ azure/init-db/init-db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Crée l'utilisateur et la base de données avec mongosh
+mongosh <<EOF
+use netflix
+db.createUser({
+  user: "attendee",
+  pwd: "apppassword",
+  roles: [
+    { role: "readWrite", 
+      db: "netflix" 
+    }
+  ]
+})
+EOF
+
+# Importe les données du fichier JSON
+mongoimport --db netflix --collection series --file /tmp/series.json --jsonArray
