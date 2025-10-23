f90df44847af6363cf99088adbe2ffee9fa62660
antoinethirion <antoine.thirion@wavestone.com>
2025-10-23 09:01:51 +0200

    chore: remove terraform unused code

--- azure/main.tf
+++ azure/main.tf
@@ -1,27 +1,3 @@
-# resource "azurerm_cosmosdb_account" "netflix-db" {
-#   name                = "netflix-db"
-#   location            = local.region
-#   resource_group_name = local.rg_name
-#   offer_type          = "Standard"
-#   kind                = "MongoDB"
-
-#   free_tier_enabled          = true
-#   automatic_failover_enabled = false
-
-#   consistency_policy {
-#     consistency_level       = "BoundedStaleness"
-#     max_interval_in_seconds = 300
-#     max_staleness_prefix    = 100000
-#   }
-
-#   # primary region
-#   geo_location {
-#     location          = local.region
-#     failover_priority = 0
-#   }
-# }
-
-
 resource "azurerm_log_analytics_workspace" "netflix-logs" {
   name                = "netflix-logs"
   location            = local.region
